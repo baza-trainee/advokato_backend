9efb2ff1fd2cbd36449e343aae447d6d6f0ef6a6
smile4alice <smile.to.alice@gmail.com>
2023-12-13 18:16:15 +0200

    feat: add new logic to schedule CRUD (multiple)

--- calendarapi/admin/schedule.py
+++ calendarapi/admin/schedule.py
@@ -1,118 +1,164 @@
-from datetime import datetime
+from datetime import datetime, time, timedelta
 import json
 
-from flask import Response, abort, request, redirect, url_for
+from flask import Response, abort, request
 from flask_admin import expose
 from flask_admin.form.validators import CustomFieldListInputRequired
-from sqlalchemy import and_, func, or_
+from markupsafe import Markup
+from sqlalchemy import and_
 from wtforms import DateField, ValidationError
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Optional
 from sqlalchemy.orm import joinedload
-
 from calendarapi.admin.common import AdminModelView
-from calendarapi.models.city import City
-from calendarapi.models.lawyer import Lawyer
 from calendarapi.models.schedule import Schedule
 from calendarapi.extensions import db
 
 
-# example for validators with args
-class MaxItemsValidator:
-    def __init__(self, max_items):
-        self.max_items = max_items
+def validate_end_date(form, field):
+    if form.data["end_date"] and form.data["end_date"] < form.data["date"]:
+        raise ValidationError("Кінцева дата не може бути раніше початкової.")
 
-    def __call__(self, form, field):
-        if field.data and len(field.data) > self.max_items:
-            raise ValidationError(f"Можна обрати не більше {self.max_items}.")
+
+def validate_date(form, field):
+    list_times = _validate_time_format(form.data["time"], skip_raise=True)
+    current_date = datetime.now().date()
+    if form.data["date"] < current_date:
+        raise ValidationError("Дата не повинна бути в минулому.")
+
+    list_dates = [form.data["date"]]
+    if form.data["end_date"]:
+        list_dates += [
+            form.data["date"] + timedelta(days=day)
+            for day in range((form.data["end_date"] - form.data["date"]).days + 1)
+        ]
+
+    lawyers_shedule = []
+    shedule_error = []
+    if list_times:
+        form._fields["time"].data = [time.strftime("%H:%M") for time in list_times]
+        for lawyer in form.data["lawyers"]:
+            existing_schedule = (
+                db.session.query(Schedule)
+                .where(
+                    and_(Schedule.lawyer_id == lawyer.id, Schedule.date.in_(list_dates))
+                )
+                .all()
+            )
+            existing_schedule = set(lawyer.date for lawyer in existing_schedule)
+            success_shedule = set(list_dates) - existing_schedule
+            if not success_shedule and (
+                form._obj and form._obj.date in existing_schedule
+            ):
+                success_shedule = [form._obj.date]
+                existing_schedule -= {form._obj.date}
+            for date in success_shedule:
+                shedule = Schedule(
+                    lawyer_id=lawyer.id, time=list_times, date=date, lawyers=[lawyer]
+                )
+                lawyers_shedule.append(shedule)
+            if existing_schedule:
+                shedule_error.append(
+                    {
+                        "lawyer": lawyer,
+                        "existing_schedule": existing_schedule,
+                    }
+                )
+        if shedule_error:
+            err = ""
+            for shedule in shedule_error:
+                if shedule != shedule_error[0]:
+                    err += "<li>"
+                err += (
+                    f"У спеціаліста <b>{shedule['lawyer']}</b> вже були записи на:<br>"
+                )
+                for date in shedule["existing_schedule"]:
+                    err += f"- {date}<br>"
+                if shedule != shedule_error[0]:
+                    err += "</li>"
+            err += f"<br>Оберіть іншу дату або видаліть існуючу у розкладі"
+            raise ValidationError(Markup(err))
+
+        if lawyers_shedule:
+            form_model = lawyers_shedule.pop()
+            form.date.data = form_model.date
+            form.lawyers.data = form_model.lawyers
+            form.lawyers.data = form_model.lawyers
+            form.time.data = form_model.time
+            db.session.add_all(lawyers_shedule)
 
 
 # example for validators without args
 def validate_time_format(form, field):
-    _validate_time_format(field.data)
+    res = _validate_time_format(field.data)
+    return res
 
 
-def _validate_time_format(time_list):
+def _validate_time_format(time_list: list, skip_raise=False) -> list[time]:
     try:
-        res = list()
-        for time in time_list:
-            if time.count(":") > 1:
-                time_format = "%H:%M:%S"
-            elif time.count(":") == 1:
-                time_format = "%H:%M"
-            else:
-                time_format = "%H"
-            res.append(datetime.strptime(time, time_format).time())
-        return res
-
+        if isinstance(time_list[0], str):
+            res = list()
+            for time in time_list:
+                if time.count("-") == 1:
+                    start_time, end_time = map(int, time.split("-"))
+                    if end_time < start_time:
+                        raise SyntaxError(f"{start_time}-{end_time}")
+                    time_list.extend(map(str, range(start_time, end_time + 1)))
+                    continue
+                if time.count(":") > 1:
+                    time_format = "%H:%M:%S"
+                elif time.count(":") == 1:
+                    time_format = "%H:%M"
+                else:
+                    time_format = "%H"
+                res.append(datetime.strptime(time, time_format).time())
+            res = list(set(res))
+            res.sort()
+            return res
+    except SyntaxError as exc:
+        if not skip_raise:
+            raise ValidationError(f"{exc.msg} <start_HH> не може бути менше <end_HH>")
+        else:
+            return False
     except ValueError:
-        raise ValidationError(
-            "Невірний формат. Приймається час у вигляді 'HH:MM:SS' або 'HH:MM' або 'HH'."
-        )
-
-
-def validate_lawyers_for_date(form, field):
-    lawyers = form.data.get("lawyers")
-
-    lawyer_id = form.data["lawyers"][0].id if lawyers else None
-    date = form.data.get("date")
-    schedule_id = (
-        form._obj.id if form._obj else None
-    )  # _obj - old object from edit form
-    existing_schedule = Schedule.query.filter_by(
-        lawyer_id=lawyer_id, date=date
-    ).one_or_none()
-
-    if existing_schedule and existing_schedule.id != schedule_id:
-        raise ValidationError(
-            f"У {existing_schedule.lawyers[0]} вже є створена запис на {date}"
-        )
-
-
-def validate_date_not_lower_than_current(form, field):
-    current_date = datetime.now().date()
-    if not field.data or field.data < current_date:
-        raise ValidationError("Оберіть вірну дату.")
+        if not skip_raise:
+            raise ValidationError(
+                "Невірний формат. Приймається час у вигляді 'HH:MM:SS' або 'HH:MM' або 'HH' або start_HH-end_HH."
+            )
+        else:
+            return False
 
 
 class ScheduleModelView(AdminModelView):
     can_set_page_size = True
-    list_template = "admin/custom_list.html"
-    current_city = "Оберіть місто"
+    # list_template = "admin/custom_list.html"
+    # current_lawyer = "Оберіть юриста"
 
-    def _reset_current_city(self):
-        self.current_city = "Оберіть місто"
+    # def _reset_current_lawyer(self):
+    #     self.current_lawyer = "Оберіть юриста"
 
-    def get_cities(self):
-        cities = db.session.query(City).all()
-        return cities
+    # def get_lawyers(self):
+    #     lawyers = db.session.query(Lawyer).all()
+    #     return lawyers
 
-    @expose("/", methods=["POST"])
-    def get_selected_city(self):
-        selected_city = request.form.get("city")
+    # @expose("/", methods=["POST"])
+    # def get_selected_lawyer(self):
+    #     selected_lawyer = request.form.get("lawyer")
 
-        if selected_city == "Усі міста" or not selected_city:
-            self._reset_current_city()
-            selected_city = None
+    #     if selected_lawyer == "Усі юристи" or not selected_lawyer:
+    #         self._reset_current_lawyer()
+    #         selected_lawyer = None
 
-        self.current_city = selected_city
-        return redirect(
-            f"?city={selected_city}"
-            if selected_city
-            else url_for("schedule.get_selected_city")
-        )
+    #     self.current_lawyer = selected_lawyer
+    #     return redirect(f"?lawyer={selected_lawyer}" if selected_lawyer else url_for("schedule.get_selected_lawyer"))
 
-    def get_query(self):
-        self.selected_city = request.args.get("city")
-        if self.selected_city:
-            self.query = db.session.query(Schedule).filter(
-                Schedule.lawyers.any(
-                    Lawyer.cities.any(City.city_name == self.selected_city)
-                )
-            )
-        else:
-            self._reset_current_city()
-            self.query = db.session.query(Schedule)
-        return self.query
+    # def get_query(self):
+    #     self.selected_lawyer = request.args.get("lawyer")
+    #     if self.selected_lawyer:
+    #         self.query = db.session.query(Schedule).filter(Schedule.lawyers.any(Lawyer.name == self.selected_lawyer))
+    #     else:
+    #         self._reset_current_lawyer()
+    #         self.query = db.session.query(Schedule)
+    #     return self.query
 
     @expose("/ajax/lookup/")
     def ajax_lookup(self):
@@ -136,7 +182,6 @@ class ScheduleModelView(AdminModelView):
         #                 Lawyer.cities.any(City.city_name == selected_city),
         #                 or_(
         #                     Lawyer.name.ilike(f"%{query}%"),
-        #                     Lawyer.surname.ilike(f"%{query}%"),
         #                 ),
         #             )
         #         )
@@ -153,9 +198,8 @@ class ScheduleModelView(AdminModelView):
         return Response(json.dumps(data), mimetype="application/json")
 
     column_labels = {
-        "lawyers": "Адвокат",
-        "lawyers.name": "Ім'я",
-        "lawyers.surname": "Прізвище",
+        "lawyers": "Спеціалісти",
+        "lawyers.name": "Спеціаліст",
         "time": "Доступний час",
         "date": "Дата",
     }
@@ -176,7 +220,7 @@ class ScheduleModelView(AdminModelView):
         execute=True,
         page_size=None,
     ):
-        selected_city = request.args.get("city")
+        selected_lawyer = request.args.get("lawyer")
 
         # Will contain join paths with optional aliased object
         joins = {}
@@ -199,21 +243,13 @@ class ScheduleModelView(AdminModelView):
             )
 
         # Apply filters
-        if selected_city and selected_city != "all":
-            count_query = (
-                self.session.query(func.count("*"))
-                .select_from(self.model)
-                .filter(
-                    Schedule.lawyers.any(
-                        Lawyer.cities.any(City.city_name == self.selected_city)
-                    )
-                )
-            )
-            query = query.filter(
-                Schedule.lawyers.any(
-                    Lawyer.cities.any(City.city_name == self.selected_city)
-                )
-            )
+        # if selected_lawyer and selected_lawyer != "all": #TODO (для відображення кількості в СПИСОК)
+        #     count_query = (
+        #         self.session.query(func.count("*"))
+        #         .select_from(self.model)
+        #         .filter(Schedule.lawyers.any(Lawyer.cities.any(City.city_name == self.selected_lawyer)))
+        #     )
+        #     query = query.filter(Schedule.lawyers.any(Lawyer.name == self.selected_lawyer))
 
         # Calculate number of rows if necessary
         count = count_query.scalar() if count_query else None
@@ -253,31 +289,36 @@ class ScheduleModelView(AdminModelView):
 
     column_searchable_list = [
         "lawyers.name",
-        "lawyers.surname",
     ]
 
     form_columns = [
         "lawyers",
         "date",
+        "end_date",
         "time",
     ]
 
     form_ajax_refs = {
         "lawyers": {
             "fields": ("name",),
-            "placeholder": "Оберіть адвоката",
+            "placeholder": "Оберіть спеціаліста",
             "minimum_input_length": 0,
         },
     }
-
+    column_descriptions = {"date": "test"}
     form_extra_fields = {
         "date": DateField(
             label="Дата",
             validators=[
-                validate_lawyers_for_date,
-                validate_date_not_lower_than_current,
+                DataRequired(),
+                validate_date,
             ],
             default=datetime.now().date(),
+        ),
+        "end_date": DateField(
+            label="Кінцева дата",
+            validators=[Optional(), validate_end_date],
+            description="Якщо бажаєте додати розклад для певного проміжку часу, а не на 1 день",
         ),
     }
 
@@ -291,10 +332,9 @@ class ScheduleModelView(AdminModelView):
 
     form_args = {
         "lawyers": {
-            "label": "Адвокат",
+            "label": "Спеціалісти",
             "validators": [
                 DataRequired(message="Це поле обов'язкове."),
-                MaxItemsValidator(max_items=1),
             ],
         },
         "time": {
@@ -304,12 +344,20 @@ class ScheduleModelView(AdminModelView):
             ],
         },
     }
+    column_filters = [
+        "date",
+    ]
 
     def on_model_change(self, form, model, is_created):
-        if form.data["lawyers"]:
-            for lawyer in form.data["lawyers"]:
-                model.lawyer_id = lawyer.id  # save to db
-        if form.data["time"]:
-            res = list(set(_validate_time_format(form.data["time"])))
-            res.sort()
-            model.time = res
+        model.lawyer_id = model.lawyers[0].id
+        return super().on_model_change(form, model, is_created)
+
+    def on_form_prefill(self, form, id):
+        if form._fields["time"].data and isinstance(form._fields["time"].data[0], time):
+            form._fields["time"].data = (
+                [time.strftime("%H:%M") for time in form.time.data]
+                if form.time.data
+                else []
+            )
+        del form._fields["end_date"]
+        return super().on_form_prefill(form, id)
