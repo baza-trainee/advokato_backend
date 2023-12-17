6de72b62a95d7e70c3d172c8248655dc188963ac
smile4alice <smile.to.alice@gmail.com>
2023-12-18 00:22:16 +0200

    feat: add coordinates to admin panel

--- calendarapi/admin/city.py
+++ calendarapi/admin/city.py
@@ -1,5 +1,26 @@
+from markupsafe import Markup
+from wtforms import StringField, ValidationError
 from calendarapi.admin.base_admin import AdminModelView
 from calendarapi.admin.commons.formatters import format_as_markup
+
+
+def validate_float(form, field):
+    try:
+        field.data = field.data.strip().replace(",", ".")
+        float(field.data)
+    except Exception as exc:
+        print(exc)
+        raise ValidationError("Невірний формат для координат. Приймаються лише дробові та цілі числа.")
+
+
+def format_coords(view, context, model, name):
+    res = '<div class="city_coordinates">'
+    coords = [model.latitude, model.longitude]
+    if all(coords):
+        res += f'<span class="city_coord_name"> Широта:</span><span class="city_coord_value">{model.latitude}</span>'
+        res += f'<span class="city_coord_name"> Довгота:</span><span class="city_coord_value">{model.longitude}</span>'
+    res += "</div>"
+    return Markup(res)
 
 
 class CityModelView(AdminModelView):
@@ -12,15 +33,22 @@ class CityModelView(AdminModelView):
     column_labels = {
         "city_name": "Місто",
         "address": "Адреса філіалу",
+        "coordinates": "Координати",
     }
 
     column_list = [
         "city_name",
         "address",
+        "coordinates",
     ]
 
     column_formatters = {
         "address": format_as_markup,
+        "coordinates": format_coords,
+    }
+    form_extra_fields = {
+        "latitude": StringField("Координати (Широта)", validators=[validate_float]),
+        "longitude": StringField("Координати (Довгота)", validators=[validate_float]),
     }
 
     column_sortable_list = []
--- calendarapi/models/city.py
+++ calendarapi/models/city.py
@@ -8,7 +8,9 @@ class City(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     city_name = db.Column(db.String(100), unique=True, nullable=False)
     address = db.Column(db.String(200), nullable=False)
-
+    latitude = db.Column(db.Float)
+    longitude = db.Column(db.Float)
+    
     def __repr__(self):
         return f"{self.city_name}"
 
